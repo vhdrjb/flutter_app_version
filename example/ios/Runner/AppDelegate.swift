d000f841ed4c46e3c9afe0fd462ca3d12943f4e3
vhdrjbs@gmail.com <Faraz@Gitlab_112233>
2021-06-09 12:26:16 +0430

    fix ios plugin.

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,25 +7,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let appVersionChannel = FlutterMethodChannel(name: "ir.vhdrjb.channel/app_version",
-                                              binaryMessenger: controller.binaryMessenger)
-    appVersionChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-        guard call.method == "getApplicationVersion" else {
-          result(FlutterMethodNotImplemented)
-          return
-        }
-        self.getAppVersion(result: result)
-    })
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     
-    private func getAppVersion(result: FlutterResult) {
-        let nsObject = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
-       let version = nsObject as! String
-        result(version)
-      }
+
 }
