2ca36d620caddcf7c4d5523a736c6c7320d9c537
vhdrjbs@gmail.com <Faraz@Gitlab_112233>
2021-06-09 12:26:16 +0430

    fix ios plugin.

--- ios/Classes/SwiftAppVersionPlugin.swift
+++ ios/Classes/SwiftAppVersionPlugin.swift
@@ -3,12 +3,22 @@ import UIKit
 
 public class SwiftAppVersionPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "app_version", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "ir.vhdrjb.channel/app_version", binaryMessenger: registrar.messenger())
     let instance = SwiftAppVersionPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    guard call.method == "getApplicationVersion" else {
+      result(FlutterMethodNotImplemented)
+      return
+    }
+    self.getAppVersion(result: result)
   }
+    
+    private func getAppVersion(result: FlutterResult) {
+        let nsObject = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
+       let version = nsObject as! String
+        result(version)
+      }
 }
